15097555768775b7ece079c27f040e6a24571691
Gentian Gashi <genti_gashi8@hotmail.com>
2022-10-22 04:40:00 +0100

    Updated to include blink animation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,71 @@
-    // navigation bar
-    const doc = document;
-    const menuOpen = doc.querySelector(".menu");
-    const menuClose = doc.querySelector(".close");
-    const overlay = doc.querySelector(".overlay");
+// Blink Animation
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid}";
+        document.body.appendChild(css);
+    };
+
+// navigation bar
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+   
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
     
-    menuOpen.addEventListener("click", () => {
-      overlay.classList.add("overlay--active");
-    });
-    
-    menuClose.addEventListener("click", () => {
-      overlay.classList.remove("overlay--active");
-    });
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
